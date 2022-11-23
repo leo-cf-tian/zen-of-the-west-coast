c525b9181e87c9661517b2fbb03c8cf282b2a5f2
Zain Salman <zainsalman04@gmail.com>
2022-11-23 17:18:05 -0500

    fixed code

--- src/util/actuator/actuator.ino
+++ src/util/actuator/actuator.ino
@@ -1,8 +1,11 @@
-int ENA = 7;
-int IN1 = 6;
-int IN2 = 5;
+// constants won't change
+const int ENA_PIN = 7; // the Arduino pin connected to the EN1 pin L298N
+const int IN1_PIN = 6; // the Arduino pin connected to the IN1 pin L298N
+const int IN2_PIN = 5; // the Arduino pin connected to the IN2 pin L298N
+
 int TRIG = 12;
 int ECHO = 11;
+int LED = 8;
 
 long getDistance(){
   digitalWrite(TRIG, LOW);
@@ -16,34 +19,29 @@ long getDistance(){
   return (duration/2)/29.1;
 }
 
-void extend_actuator(int time){
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-
-  delay(time);
-
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
-}
-
 void setup() {
-  Serial.begin(9600);
+  pinMode(ENA_PIN, OUTPUT);
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
 
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
 
-  pinMode(LED, OUTPUT);
-  pinMode(ENA, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-
-  digitalWrite(ENA, HIGH);
+  digitalWrite(ENA_PIN, HIGH);
 }
 
 void loop() {
   long distance = getDistance();
-  if(distance <= 15)
-  {
-    extend_actuator(2000);
+  if(distance <= 15) {
+    digitalWrite(IN1_PIN, LOW);
+    digitalWrite(IN2_PIN, HIGH);
+
+    delay(20000); // actuator will stop extending automatically when reaching the limit
+
+    // retracts the actuator
+    digitalWrite(IN1_PIN, HIGH);
+    digitalWrite(IN2_PIN, LOW);
+
+    delay(20000); // actuator will stop retracting automatically when reaching the limit
   }
 }
---
+++ src/util/sketch_nov17b/sketch_nov17b.ino
@@ -0,0 +1,44 @@
+int ENA = 7;
+int IN1 = 6;
+int IN2 = 5;
+int TRIG = 12;
+int ECHO = 11;
+
+long getDistance(){
+  digitalWrite(TRIG, LOW);
+  delay(50);
+  digitalWrite(TRIG, HIGH);
+  delay(100);
+  digitalWrite(TRIG, LOW);
+
+  pinMode(ECHO, INPUT);
+  long duration = pulseIn(ECHO, HIGH);
+  return (duration/2)/29.1;
+}
+
+void extend_actuator(int time){
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+
+  delay(time);
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(TRIG, OUTPUT);
+  pinMode(ECHO, INPUT);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  digitalWrite(ENA, HIGH);
+}
+
+void loop() {
+  extend_actuator(2000);
+}
